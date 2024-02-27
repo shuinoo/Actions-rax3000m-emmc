662abcfb0e8f6df2fac665a6247585a8c1d7a5db
caijinshui <jinshui.cai@sunisco.com>
2024-02-27 17:09:25 +0800

    新增软件源

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,3 +2,5 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+echo "src-git custom https://github.com/shuinoo/op_package.git" >> feeds.conf.default
