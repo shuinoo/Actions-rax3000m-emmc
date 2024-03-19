b7307cd955e9c336dbe66a9dee520745b523b40d
caijinshui <jinshui.cai@sunisco.com>
2024-03-19 11:30:54 +0800

    修改

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "src-git custom https://github.com/shuinoo/op_package.git;lede" >> feeds.conf.default
+echo "src-git custom https://github.com/shuinoo/op_package.git" >> feeds.conf.default
